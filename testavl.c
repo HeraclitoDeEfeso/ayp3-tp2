ead56ce52fa4f712b7ae9f4e19b7889920fe8d42
MatiasMonti <33459048+MatiasMonti@users.noreply.github.com>
2019-06-01 17:35:09 -0300

    test pára los metodos publicos listos
    faltan implementar los de rotacion

--- testavl.c
+++ testavl.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include "avl.h"
+#include <stdio.h>
 
 void testCrearUnArbol(){
     Arbol *arbol = NULL;
@@ -15,7 +16,6 @@ void testCrearUnArbolConDosNodosUnoADerecha(){
     assert(arbol->clave == 5);
     assert(arbol->hijos[DERECHA] != NULL);
 }
-
 void testBuscarUnaClave()
 {
     Arbol *arbol = NULL;
@@ -29,11 +29,116 @@ void testBuscarUnaClave()
 //    while (borrar) {
 //        free((Arbol *) borrar->dato);
 //        borrar = desapilarLista(borrar);
-//    }
+// }
 }
 
+
+void testBorrarLaRaizDeUnArbolConUnElemento(){
+    Arbol *arbol = NULL;
+    arbol = insertarArbol(arbol, 5);
+    borrarArbol(arbol, 5);
+    assert(arbol == NULL);
+}
+
+void testBorrarLaRaizDeUnArbolConMasDeUnElemento(){
+    Arbol *arbol = NULL;
+    arbol = insertarArbol(arbol, 5);
+    arbol = insertarArbol(arbol, 8);
+    borrarArbol(arbol, 5);
+    assert(arbol->clave == 8);
+}
+
+void testBorrarUnElementoEnLaMitadDelArbol(){
+    Arbol *arbol = NULL;
+    arbol = insertarArbol(arbol, 5);
+    arbol = insertarArbol(arbol, 8);
+    arbol = insertarArbol(arbol, 18);
+    borrarArbol(arbol, 8);
+    assert(buscarArbol(arbol,8) == NULL);
+    assert(buscarArbol(arbol,18) != NULL);
+    assert(arbol->clave == 5);
+}
+
+void testInOrder(){
+    Arbol *arbol = NULL;
+    arbol = insertarArbol(arbol, 5);
+    arbol = insertarArbol(arbol, 8);
+    arbol = insertarArbol(arbol, 18);
+    arbol = insertarArbol(arbol, 4);
+    arbol = insertarArbol(arbol, 2);
+    enordenArbol(arbol);
+    Lista *lista ;
+    lista = postordenArbol(arbol);
+    Lista *ordenada = NULL;
+    apilarLista(ordenada,(int *) 2);
+    apilarLista(ordenada,(int *) 4);
+    apilarLista(ordenada,(int *) 5);
+    apilarLista(ordenada,(int *) 8);
+    apilarLista(ordenada,(int *) 18);
+    while(ordenada){
+        assert(lista->dato == ordenada->dato);
+        desapilarLista(ordenada);
+        desapilarLista(lista);
+    }
+}
+void testPreOrder(){
+    Arbol *arbol = NULL;
+    arbol = insertarArbol(arbol, 5);
+    arbol = insertarArbol(arbol, 8);
+    arbol = insertarArbol(arbol, 18);
+    arbol = insertarArbol(arbol, 4);
+    arbol = insertarArbol(arbol, 2);
+    enordenArbol(arbol);
+    Lista *lista ;
+    lista = postordenArbol(arbol);
+    Lista *ordenada = NULL;
+    apilarLista(ordenada,(int *) 5);
+    apilarLista(ordenada,(int *) 4);
+    apilarLista(ordenada,(int *) 2);
+    apilarLista(ordenada,(int *) 8);
+    apilarLista(ordenada,(int *) 18);
+    while(ordenada){
+        assert(lista->dato == ordenada->dato);
+        desapilarLista(ordenada);
+        desapilarLista(lista);
+    }
+}void testPostOrder(){
+    Arbol *arbol = NULL;
+    arbol = insertarArbol(arbol, 5);
+    arbol = insertarArbol(arbol, 8);
+    arbol = insertarArbol(arbol, 18);
+    arbol = insertarArbol(arbol, 4);
+    arbol = insertarArbol(arbol, 2);
+    enordenArbol(arbol);
+    Lista *lista ;
+    lista = postordenArbol(arbol);
+    Lista *ordenada = NULL;
+    apilarLista(ordenada,(int *) 2);
+    apilarLista(ordenada,(int *) 4);
+    apilarLista(ordenada,(int *) 18);
+    apilarLista(ordenada,(int *) 8);
+    apilarLista(ordenada,(int *) 5);
+    while(ordenada){
+        assert(lista->dato == ordenada->dato);
+        desapilarLista(ordenada);
+        desapilarLista(lista);
+    }
+}
+
+
+
+
+
 int main(){
+    printf("asadas");
     testCrearUnArbol();
     testCrearUnArbolConDosNodosUnoADerecha();
+    testBuscarUnaClave();
+    testBorrarLaRaizDeUnArbolConMasDeUnElemento();
+    testBorrarLaRaizDeUnArbolConUnElemento();
+    testBorrarUnElementoEnLaMitadDelArbol();
+    testInOrder();
+    testPostOrder();
+    testPreOrder();
     return 0;
 }
