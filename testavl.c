1c040c427014705e9ca1456cea49f955f30ab9dd
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2019-06-07 15:20:51 -0300

    Test: corrijo y agrego auxiliares

--- testavl.c
+++ testavl.c
@@ -1,17 +1,60 @@
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "avl.h"
 
-void testCrearUnArbol(){
+void mostrarArbol(Arbol *arbol, int level)
+{
+    if (arbol) {
+        printf("%.4i(%2i)\n", arbol->clave, arbol->balance);
+        if (arbol->izquierda) {
+            printf("%*s", 4 * level + 4, "izq:" );
+            mostrarArbol(arbol->izquierda, level + 1);
+        }
+        if (arbol->derecha) {
+            printf("%*s", 4 * level + 4, "der:" );
+            mostrarArbol(arbol->derecha, level + 1);
+        }
+    } else {
+        printf("\n");
+    }
+}
+
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+int altura(Arbol *arbol)
+{
+    return (arbol != NULL)
+           ? 1 + max(altura(arbol->izquierda), altura(arbol->derecha))
+           : 0;
+}
+
+int esAVL(Arbol *arbol)
+{
+    return  arbol == NULL
+            || (abs(altura(arbol->izquierda) - altura(arbol->derecha)) <= 1
+                && esAVL(arbol->izquierda)
+                && esAVL(arbol->derecha));
+}
+
+void testCrearUnArbol()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 5);
+    assert(esAVL(arbol));
     assert(arbol->clave == 5);
 }
 
-void testCrearUnArbolConDosNodosUnoADerecha(){
+void testCrearUnArbolConDosNodosUnoADerecha()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 5);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 7);
+    assert(esAVL(arbol));
     assert(arbol->clave == 5);
     assert(arbol->derecha->clave == 7);
 }
@@ -20,45 +63,66 @@ void testBuscarUnaClave()
 {
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 5);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 4);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 1);
+    assert(esAVL(arbol));
     assert(buscarArbol(arbol,12) == NULL);
     assert(buscarArbol(arbol,1) != NULL);
 }
 
-void testBorrarLaRaizDeUnArbolConUnElemento(){
+void testBorrarLaRaizDeUnArbolConUnElemento()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 5);
+    assert(esAVL(arbol));
     arbol = borrarArbol(arbol, 5);
+    assert(esAVL(arbol));
     assert(arbol == NULL);
 }
 
-void testBorrarLaRaizDeUnArbolConMasDeUnElemento(){
+void testBorrarLaRaizDeUnArbolConMasDeUnElemento()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 5);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 8);
+    assert(esAVL(arbol));
     arbol = borrarArbol(arbol, 5);
+    assert(esAVL(arbol));
     assert(arbol->clave == 8);
 }
 
-void testBorrarUnElementoEnLaMitadDelArbol(){
+void testBorrarUnElementoEnLaMitadDelArbol()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 5);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 8);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 18);
+    assert(esAVL(arbol));
     arbol = borrarArbol(arbol, 8);
-    assert(buscarArbol(arbol,8) == NULL);
-    assert(buscarArbol(arbol,18) != NULL);
+    assert(esAVL(arbol));
+    assert(buscarArbol(arbol, 8) == NULL);
+    assert(buscarArbol(arbol, 18) != NULL);
     assert(arbol->clave == 5);
 }
 
-void testInOrder(){
+void testInOrder()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 5);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 8);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 18);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 4);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 2);
+    assert(esAVL(arbol));
     Lista *lista = enordenArbol(arbol);
     Lista *ordenada = NULL;
     apilarLista(ordenada,(int *) 2);
@@ -72,13 +136,20 @@ void testInOrder(){
         desapilarLista(lista);
     }
 }
-void testPreOrder(){
+
+void testPreOrder()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 5);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 8);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 18);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 4);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 2);
+    assert(esAVL(arbol));
     Lista *lista = preordenArbol(arbol);
     Lista *ordenada = NULL;
     apilarLista(ordenada,(int *) 5);
@@ -93,13 +164,19 @@ void testPreOrder(){
     }
 }
 
-void testPostOrder(){
+void testPostOrder()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 5);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 8);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 18);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 4);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 2);
+    assert(esAVL(arbol));
     Lista *lista = postordenArbol(arbol);
     Lista *ordenada = NULL;
     apilarLista(ordenada,(int *) 2);
@@ -114,100 +191,148 @@ void testPostOrder(){
     }
 }
 
-void testArbolConUnElementoEsAvl(){
+void testArbolConUnElementoEsAvl()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol,5);
-    assert(abs(arbol->balance) <= 1);
+    assert(esAVL(arbol));
 }
 
-void testBalanceoSimpleIzquierda(){
+void testBalanceoSimpleIzquierda()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 5);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 8);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 18);
+    assert(esAVL(arbol));
     assert(arbol->clave == 8);
-    assert(abs(arbol->balance) <= 1);
 }
 
-void testBalanceoSimpleDerecha(){
+void testBalanceoSimpleDerecha()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 5);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 4);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 1);
     assert(arbol->clave == 4);
-    assert(abs(arbol->balance) <= 1);
+    assert(esAVL(arbol));
 }
 
-void testBalanceoDobleALaDerecha(){
+void testBalanceoDobleALaDerecha()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 18);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 14);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 15);
     assert(arbol->clave == 15);
-    assert(abs(arbol->balance) <= 1);
+    assert(esAVL(arbol));
 }
 
-void testBalanceoDobleALaIzquierda(){
+void testBalanceoDobleALaIzquierda()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 8);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 4);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 6);
     assert(arbol->clave == 6);
-    assert(abs(arbol->balance) <= 1);
+    assert(esAVL(arbol));
 }
 
-void testBalanceoaLaDerechaDespuesDeBorrar(){
+void testBalanceoaLaDerechaDespuesDeBorrar()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 8);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 4);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 9);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 3);/*hasta aca esta balanceado*/
+    assert(esAVL(arbol));
     arbol = borrarArbol(arbol, 9);
     assert(arbol->clave == 4);
-    assert(arbol->derecha == 8);
-    assert(arbol->izquierda == 3);
-    assert(abs(arbol->balance) <= 1);
+    assert(arbol->derecha->clave == 8);
+    assert(arbol->izquierda->clave == 3);
+    assert(esAVL(arbol));
 }
 
-void testBalanceoaLaIzquierdaDespuesDeBorrar(){
+void testBalanceoaLaIzquierdaDespuesDeBorrar()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 8);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 4);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 9);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 11);/*hasta aca esta balanceado*/
+    assert(esAVL(arbol));
     arbol = borrarArbol(arbol, 4);
     assert(arbol->clave == 9);
-    assert(arbol->derecha == 11);
-    assert(arbol->izquierda == 8);
-    assert(abs(arbol->balance) <= 1);
+    assert(arbol->derecha->clave == 11);
+    assert(arbol->izquierda->clave == 8);
+    assert(esAVL(arbol));
 }
 
-void testBalanceoDobleALaIzquierdaDespuesDeBorrar(){
+void testBalanceoDobleALaIzquierdaDespuesDeBorrar()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 8);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 4);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol,13);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 11);/*hasta aca esta balanceado*/
+    assert(esAVL(arbol));
     arbol = borrarArbol(arbol, 4);
-    assert(arbol->clave == 8);
-    assert(arbol->derecha == 13);
-    assert(arbol->izquierda == 11);
-    assert(abs(arbol->balance) <= 1);
+    assert(arbol->clave == 11);
+    assert(arbol->derecha->clave == 13);
+    assert(arbol->izquierda->clave == 8);
+    assert(esAVL(arbol));
 }
-void testBalanceoDobleALaDerechaDespuesDeBorrar(){
+
+void testBalanceoDobleALaDerechaDespuesDeBorrar()
+{
     Arbol *arbol = NULL;
     arbol = insertarArbol(arbol, 8);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 4);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol,13);
+    assert(esAVL(arbol));
     arbol = insertarArbol(arbol, 5);/*hasta aca esta balanceado*/
+    assert(esAVL(arbol));
     arbol = borrarArbol(arbol, 13);
     assert(arbol->clave == 5);
-    assert(arbol->derecha == 8);
-    assert(arbol->izquierda == 4);
-    assert(abs(arbol->balance) <= 1);
+    assert(arbol->derecha->clave == 8);
+    assert(arbol->izquierda->clave == 4);
+    assert(esAVL(arbol));
 }
 
-int main(){
+void testBorradoPrecisaDobleBalanceo()
+{
+    Arbol *arbol = NULL;
+    int i, numeros[] = {50, 25, 75, 80, 60, 40, 15, 90, 65, 55, 35, 62};
+    for(i = 0; i < sizeof(numeros) / sizeof(int); i++) {
+        arbol = insertarArbol(arbol, numeros[i]);
+        assert(esAVL(arbol));
+    }
+    arbol = borrarArbol(arbol, 15);
+    assert(esAVL(arbol));
+    assert(arbol->clave == 60);
+}
+
+int main()
+{
     testCrearUnArbol();
     testCrearUnArbolConDosNodosUnoADerecha();
     testBuscarUnaClave();
@@ -226,5 +351,6 @@ int main(){
     testBalanceoaLaIzquierdaDespuesDeBorrar();
     testBalanceoDobleALaDerechaDespuesDeBorrar();
     testBalanceoDobleALaIzquierdaDespuesDeBorrar();
+    testBorradoPrecisaDobleBalanceo();
     return 0;
 }
